bb49adf1de5ff2690f8970ce81d3cc1a261144ae
Nadine <nadlgit@users.noreply.github.com>
2022-04-17 20:46:10 -0400

    feat: dark and light mode colors

--- src/features/layout/layout/theme-switch.jsx
+++ src/features/layout/layout/theme-switch.jsx
@@ -1,7 +1,11 @@
 // import styles from './theme-switch.module.css';
-// import { useTheme } from 'features/theme';
+import { useTheme } from 'features/theme';
 
 export const ThemeSwitch = () => {
-  // const { theme, setTheme } = useTheme();
-  return null;
+  const { theme, setTheme } = useTheme();
+  return theme === 'light' ? (
+    <button onClick={() => setTheme('dark')}>Dark mode</button>
+  ) : (
+    <button onClick={() => setTheme('light')}>Light mode</button>
+  );
 };
