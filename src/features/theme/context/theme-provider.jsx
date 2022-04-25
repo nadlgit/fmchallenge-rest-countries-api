aa1050a6dd8a27d450298eaaf6446a3cce5a8f09
Nadine <nadlgit@users.noreply.github.com>
2022-04-24 21:40:22 -0400

    feat: default to light mode + header style

--- src/features/theme/context/theme-provider.jsx
+++ src/features/theme/context/theme-provider.jsx
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react';
 
 export const ThemeContext = createContext();
 
-export const ThemeProvider = ({ children, initialTheme = 'dark' }) => {
+export const ThemeProvider = ({ children, initialTheme = 'light' }) => {
   const [theme, setTheme] = useState(initialTheme);
   return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>;
 };
