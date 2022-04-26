c1a50483ed8ed24fa4a782b43a1efcd6c802244f
Nadine <nadlgit@users.noreply.github.com>
2022-04-25 21:16:13 -0400

    feat: added countries filter by region

--- src/features/countries/index.js
+++ src/features/countries/index.js
@@ -1,2 +1,3 @@
 export * from './list';
 export * from './search';
+export * from './filter';
--- src/pages/results.jsx
+++ src/pages/results.jsx
@@ -1,31 +1,52 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useAllCountries, useCountriesByName, withLoading } from 'features/data';
-import { CountriesList, CountriesSearch } from 'features/countries';
+import { CountriesList, CountriesSearch, CountriesFilter } from 'features/countries';
 
 export const ResultsPage = () => {
   const { query } = useParams();
+  const [filter, setFilter] = useState('');
+  useEffect(() => {
+    setFilter('');
+  }, [query]);
   return (
     <>
       <CountriesSearch />
-      {query ? <CountriesByName query={query} /> : <AllCountries />}
+      <CountriesFilter filter={filter} onFilterChange={setFilter} />
+      {query ? <CountriesByName query={query} filter={filter} /> : <AllCountries filter={filter} />}
     </>
   );
 };
 
-const CountriesListWithLoading = ({ isLoading, error, countries }) =>
+const CountriesListWithLoading = ({ isLoading, error, countries, filter }) =>
   withLoading(CountriesList)({
     // LoadingComponent: todo,
     isLoading,
     error,
-    countries,
+    countries:
+      filter === '' ? countries : countries?.filter((country) => country?.region === filter),
   });
 
-const CountriesByName = ({ query }) => {
+const CountriesByName = ({ query, filter }) => {
   const { isLoading, error, countries } = useCountriesByName(query);
-  return <CountriesListWithLoading isLoading={isLoading} error={error} countries={countries} />;
+  return (
+    <CountriesListWithLoading
+      isLoading={isLoading}
+      error={error}
+      countries={countries}
+      filter={filter}
+    />
+  );
 };
 
-const AllCountries = () => {
+const AllCountries = ({ filter }) => {
   const { isLoading, error, countries } = useAllCountries();
-  return <CountriesListWithLoading isLoading={isLoading} error={error} countries={countries} />;
+  return (
+    <CountriesListWithLoading
+      isLoading={isLoading}
+      error={error}
+      countries={countries}
+      filter={filter}
+    />
+  );
 };
