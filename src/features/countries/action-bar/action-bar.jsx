4448579ba889506618adb4e4a0959dec97e94101
Nadine <nadlgit@users.noreply.github.com>
2022-04-25 22:14:41 -0400

    feat: CountriesActionBar component

---
+++ src/features/countries/action-bar/action-bar.jsx
@@ -0,0 +1,12 @@
+import styles from './action-bar.module.css';
+import { CountriesSearch } from '../search';
+import { CountriesFilter } from '../filter';
+
+export const CountriesActionBar = ({ filter = '', onFilterChange = (value) => {} }) => {
+  return (
+    <div className={styles.actionbar}>
+      <CountriesSearch />
+      <CountriesFilter filter={filter} onFilterChange={onFilterChange} />
+    </div>
+  );
+};
