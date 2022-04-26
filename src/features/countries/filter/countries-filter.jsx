840e20ba5b6f1a713d02b100e5e3671493119428
Nadine <nadlgit@users.noreply.github.com>
2022-04-25 21:16:13 -0400

    feat: added countries filter by region

--- src/features/countries/filter/countries-filter.jsx
+++ src/features/countries/filter/countries-filter.jsx
@@ -1,1 +1,22 @@
-export const CountriesFilter = () => {};
+import styles from './countries-filter.module.css';
+
+export const CountriesFilter = ({ filter = '', onFilterChange = (value) => {} }) => {
+  const regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  const handleChange = (e) => {
+    onFilterChange(e.target.value);
+  };
+  return (
+    <div className={styles.filter}>
+      <select onChange={handleChange}>
+        <option value="" selected={filter === ''}>
+          Filter by Region
+        </option>
+        {regions.map((item) => (
+          <option key={item} selected={filter === item}>
+            {item}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
