6b00f3d4c0530faf448050a53fa185b4cdfe17b4
Nadine <nadlgit@users.noreply.github.com>
2022-05-16 12:56:34 -0400

    feat: CountryList component style + css reset update + ChallengeAttribution component update

--- src/features/misc/fmchallenge-attribution/attribution.jsx
+++ src/features/misc/fmchallenge-attribution/attribution.jsx
@@ -6,6 +6,6 @@ export const ChallengeAttribution = () => (
     {// eslint-disable-next-line react/jsx-no-target-blank
     }Challenge by <a href="https://www.frontendmentor.io/challenges/rest-countries-api-with-color-theme-switcher-5cacc469fec04111f7b848ca" target="_blank">Frontend Mentor</a>
     {// eslint-disable-next-line jsx-a11y/anchor-is-valid
-    }. Coded by <a href="#">Nadine</a>.
+    }. Coded by <a href="https://github.com/nadlgit">Nadine</a>.
   </div>
 );
