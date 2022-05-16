cb8671d4e3bca71b9de61d2d008e60692041f236
Nadine <nadlgit@users.noreply.github.com>
2022-05-15 22:59:42 -0400

    feat: CountryCard component style

--- src/features/countries/country-card/country-card.jsx
+++ src/features/countries/country-card/country-card.jsx
@@ -1,9 +1,10 @@
 import styles from './country-card.module.css';
 import { Hyperlink } from 'shared/ui';
+import { CountryFlag } from './country-flag';
 
 export const CountryCard = ({ code, flag, name, population, region, capital }) => (
   <Hyperlink url={`/country/${code}`} className={styles.card}>
-    <img loading="lazy" src={flag} alt={`${name} flag`} />
+    <CountryFlag src={flag} alt={`${name} flag`} />
     <h2>{name}</h2>
     <p>
       <span>Population:</span> {population.toLocaleString('en-US')}
