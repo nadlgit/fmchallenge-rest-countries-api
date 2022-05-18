0dbbc10365f77328f41c2a5203c9a8e829a127ae
Nadine <nadlgit@users.noreply.github.com>
2022-05-17 21:03:38 -0400

    feat: styled CountryDetails component

--- src/features/country/country-details/country-details.jsx
+++ src/features/country/country-details/country-details.jsx
@@ -1,3 +1,4 @@
+import styles from './country-details.module.css';
 import { BsArrowLeft } from 'react-icons/bs';
 import { Hyperlink } from 'shared/ui';
 import { useNavigate } from 'react-router-dom';
@@ -5,41 +6,64 @@ import { useNavigate } from 'react-router-dom';
 export const CountryDetails = ({ country, borders }) => {
   const navigate = useNavigate();
   return (
-    <div>
-      <button onClick={() => navigate(-1)}>
+    <div className={styles.container}>
+      <button onClick={() => navigate(-1)} className={`${styles.button} ${styles.navback}`}>
         <BsArrowLeft />
         Back
       </button>
+
       <img src={country?.flag} alt={`${country?.name} flag`} />
-      <h2>{country?.name}</h2>
-      <p>
-        <span>Native Name:</span> {country?.nativeName}
-      </p>
-      <p>
-        <span>Population:</span> {country?.population?.toLocaleString('en-US')}
-      </p>
-      <p>
-        <span>Region:</span> {country?.region}
-      </p>
-      <p>
-        <span>Sub Region:</span> {country?.subregion}
-      </p>
-      <p>
-        <span>Capital:</span> {country?.capital}
-      </p>
-      <p>
-        <span>Top Level Domain:</span> {country?.topLevelDomain}
-      </p>
-      <p>
-        <span>Currencies:</span> {country?.currencies?.join(',')}
-      </p>
-      <p>
-        <span>Languages:</span> {country?.languages?.join(',')}
-      </p>
-      <h3>Border Countries:</h3>
-      {borders?.map((item) => (
-        <Hyperlink url={`/country/${item?.code}`}>{item?.name}</Hyperlink>
-      ))}
+
+      <div>
+        <h2>{country?.name}</h2>
+
+        <div className={styles.info}>
+          <div>
+            <p>
+              <span>Native Name:</span> {country?.nativeName}
+            </p>
+            <p>
+              <span>Population:</span> {country?.population?.toLocaleString('en-US')}
+            </p>
+            <p>
+              <span>Region:</span> {country?.region}
+            </p>
+            <p>
+              <span>Sub Region:</span> {country?.subregion}
+            </p>
+            <p>
+              <span>Capital:</span> {country?.capital}
+            </p>
+          </div>
+
+          <div>
+            <p>
+              <span>Top Level Domain:</span> {country?.topLevelDomain}
+            </p>
+            <p>
+              <span>Currencies:</span> {country?.currencies?.join(', ')}
+            </p>
+            <p>
+              <span>Languages:</span> {country?.languages?.join(', ')}
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.borders}>
+          <h3>Border Countries:</h3>
+
+          <div>
+            {borders?.map((item) => (
+              <Hyperlink
+                url={`/country/${item?.code}`}
+                className={`${styles.button} ${styles.navborder}`}
+              >
+                {item?.name}
+              </Hyperlink>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
