1e06fb0bdc4f68955cc5b95d7e2e7efb6417a855
Nadine <nadlgit@users.noreply.github.com>
2022-05-17 21:13:48 -0400

    feat: added message when countries list is empty

--- src/features/countries/list/countries-list.jsx
+++ src/features/countries/list/countries-list.jsx
@@ -2,17 +2,20 @@ import styles from './countries-list.module.css';
 import { CountryCard } from '../country-card';
 
 export const CountriesList = ({ countries }) => (
-  <div className={styles.list}>
-    {countries?.map((country) => (
-      <CountryCard
-        key={country?.name}
-        code={country?.code}
-        flag={country?.flag}
-        name={country?.name}
-        population={country?.population}
-        region={country?.region}
-        capital={country?.capital}
-      />
-    ))}
-  </div>
+  <>
+    {!countries?.length && <p className={styles.message}>No country match.</p>}
+    <div className={styles.list}>
+      {countries?.map((country) => (
+        <CountryCard
+          key={country?.name}
+          code={country?.code}
+          flag={country?.flag}
+          name={country?.name}
+          population={country?.population}
+          region={country?.region}
+          capital={country?.capital}
+        />
+      ))}
+    </div>
+  </>
 );
