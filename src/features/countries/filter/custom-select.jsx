1d1fc8e1aa17bcc6cc972f71cfcb8117c24b9c25
Nadine <nadlgit@users.noreply.github.com>
2022-05-22 17:14:42 -0400

    test+fix: CustomSelect component

--- src/features/countries/filter/custom-select.jsx
+++ src/features/countries/filter/custom-select.jsx
@@ -2,8 +2,11 @@ import styles from './custom-select.module.css';
 import { useState } from 'react';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 
-export const CustomSelect = ({ options = [], onChange = (value) => {} }) => {
-  const [option, setOption] = useState(options.length > 0 ? options[0] : { value: '', label: '' });
+export const CustomSelect = ({ options = [], selectedValue = '', onChange = (value) => {} }) => {
+  const [option, setOption] = useState(
+    options?.find((item) => item?.value === selectedValue) ??
+      (options.length > 0 ? options[0] : { value: '', label: '' })
+  );
   const [isClosed, setIsClosed] = useState(true);
   const handleChange = (newOption) => {
     setOption(newOption);
@@ -16,19 +19,21 @@ export const CustomSelect = ({ options = [], onChange = (value) => {} }) => {
         onClick={() => setIsClosed((value) => !value)}
         className={styles.select}
         role="combobox"
+        value={option?.value}
         aria-expanded={!isClosed}
         aria-controls=""
       >
         {option?.label}
         <MdKeyboardArrowDown />
       </button>
-      <div className={`${styles.options} ${isClosed ? styles.closed : ''}`}>
+      <div className={styles.options}>
         {options.map((item, key) => (
           <button
             key={key}
             onClick={() => handleChange(item)}
-            className={item?.value === option?.value ? styles.currentoption : ''}
             role="option"
+            value={item?.value}
+            hidden={isClosed || item?.value === option?.value}
             aria-selected={item?.value === option?.value}
           >
             {item?.label}
--- src/features/countries/filter/custom-select.test.js
+++ src/features/countries/filter/custom-select.test.js
@@ -1,1 +1,87 @@
-test.todo('custom select');
+import { CustomSelect } from './custom-select';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+describe('CustomSelect', () => {
+  const getSelectEl = () => screen.getByRole('combobox');
+  const getOptionsEl = () => screen.queryAllByRole('option');
+  const getOptionElByText = (text) => screen.queryByRole('option', { name: text });
+
+  const testOptions = Array(5)
+    .fill()
+    .map((item, key) => ({
+      value: `${key + 1}`,
+      label: `Option n°${key + 1}`,
+    }));
+  const testOnChange = jest.fn();
+  const testSelectedOption = testOptions[testOptions.length - 1];
+  const getSelectableOptions = (selectedOption) =>
+    testOptions.filter((item) => item !== selectedOption);
+
+  test('initial state with all props', () => {
+    render(
+      <CustomSelect
+        options={testOptions}
+        selectedValue={testSelectedOption.value}
+        onChange={testOnChange}
+      />
+    );
+    const expectedSelectedOption = testSelectedOption;
+    const selectEl = getSelectEl();
+    expect(selectEl).toHaveValue(expectedSelectedOption.value);
+    expect(selectEl).toHaveTextContent(expectedSelectedOption.label);
+    expect(getOptionsEl().length).toBe(0);
+
+    // Open options
+    userEvent.click(selectEl);
+    const expectedOptions = getSelectableOptions(expectedSelectedOption);
+    const optionsEl = getOptionsEl();
+    expect(optionsEl.length).toBe(expectedOptions.length);
+    optionsEl.forEach((item, key) => {
+      expect(item).toHaveValue(expectedOptions[key].value);
+      expect(item).toHaveTextContent(expectedOptions[key].label);
+    });
+
+    // Close options
+    userEvent.click(selectEl);
+    expect(selectEl).toHaveValue(expectedSelectedOption.value);
+    expect(selectEl).toHaveTextContent(expectedSelectedOption.label);
+    expect(getOptionsEl().length).toBe(0);
+  });
+
+  test('initial state without selectedValue props', () => {
+    render(<CustomSelect options={testOptions} onChange={testOnChange} />);
+    const expectedSelectedOption = testOptions[0];
+    const selectEl = getSelectEl();
+    expect(selectEl).toHaveValue(expectedSelectedOption.value);
+    expect(selectEl).toHaveTextContent(expectedSelectedOption.label);
+    expect(getOptionsEl().length).toBe(0);
+  });
+
+  test('initial state without options props', () => {
+    render(<CustomSelect selectedValue={testSelectedOption.value} onChange={testOnChange} />);
+    const selectEl = getSelectEl();
+    expect(selectEl).toHaveValue('');
+    expect(selectEl).toHaveTextContent('');
+    expect(getOptionsEl().length).toBe(0);
+  });
+
+  test('select option', () => {
+    render(
+      <CustomSelect
+        options={testOptions}
+        selectedValue={testSelectedOption.value}
+        onChange={testOnChange}
+      />
+    );
+    const testNewOption = getSelectableOptions(testSelectedOption)[testOptions.length - 2];
+    const selectEl = getSelectEl();
+    userEvent.click(selectEl);
+    userEvent.click(getOptionElByText(testNewOption.label));
+    expect(testOnChange).toHaveBeenLastCalledWith(testNewOption.value);
+    expect(selectEl).toHaveValue(testNewOption.value);
+    expect(selectEl).toHaveTextContent(testNewOption.label);
+    expect(getOptionsEl().length).toBe(0);
+  });
+});
